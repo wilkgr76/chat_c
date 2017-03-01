192ae0ef44a5c8a8fbcf3f3d4be9e964ab6d39a4
shivajiva101 <shivajiva101@hotmail.com>
2017-03-01 01:10:39 +0000

    refactor the code
    used a table to allow easier expansion of colours whilst reducing the lines of code for the task. Also added return true so the core knows it's handled and doesn't pass the original message on to the other connected clients

--- init.lua
+++ init.lua
@@ -1,25 +1,22 @@
 minetest.register_privilege("warning", {description = "Can send warning message to all players", give_to_singleplayer=false})
 
+local helper = {
+  {"r","red",3},
+  {"g","green",3},
+  {"b","blue",3},
+  {"lg","lightgreen",4},
+  {"lb","lightblue",4},
+  {"p","pink",3},
+}
+
 minetest.register_on_chat_message(function(name, message)
-	if string.find(message, "r ") == 1 then
-		local msg = core.colorize("red", string.sub(message,3));
-		minetest.chat_send_all("<" .. name .. "> " .. msg);
-	elseif string.find(message, "g ") == 1 then
-		local msg = core.colorize("green", string.sub(message,3));
-		minetest.chat_send_all("<" .. name .. "> " .. msg);
-	elseif string.find(message, "b ") == 1 then
-		local msg = core.colorize("blue", string.sub(message,3));
-		minetest.chat_send_all("<" .. name .. "> " .. msg);
-	elseif string.find(message, "lg ") == 1 then
-		local msg = core.colorize("lightgreen", string.sub(message,4));
-		minetest.chat_send_all("<" .. name .. "> " .. msg);
-	elseif string.find(message, "lb ") == 1 then
-		local msg = core.colorize("lightblue", string.sub(message,4));
-		minetest.chat_send_all("<" .. name .. "> " .. msg);
-	elseif string.find(message, "p ") == 1 then
-		local msg = core.colorize("pink", string.sub(message,3));
-		minetest.chat_send_all("<" .. name .. "> " .. msg);
-	end
+  for _, row in ipairs(helper) do
+    if string.find(message, row[1].." ") == 1 then
+	local msg = core.colorize(row[2], string.sub(message,row[3]))
+	minetest.chat_send_all("<" .. name .. "> " .. msg)
+        return true
+    end
+  end
 end)
 
 minetest.register_chatcommand("public_warning", {
